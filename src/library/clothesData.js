348b43a2e39f4043547247274aece924652a951f
Ekaterina Kazantseva <67483124+ezhik-v-tumane99@users.noreply.github.com>
2021-02-20 10:26:35 -0800

    Add files via upload
    Separate file with preset clothes categories

---
+++ src/library/clothesData.js
@@ -0,0 +1,37 @@
+const topsCategories = [
+	{ name: 'Shirts', isExpanded: false},
+	{ name: 'T-Shirts', isExpanded: false},
+	{ name: 'Sweaters', isExpanded: false},
+	{ name: 'Blazers', isExpanded: false}
+]
+
+const botsCategories = [
+	{ name: 'Pants', isExpanded: false},
+	{ name: 'Shorts', isExpanded: false},
+	{ name: 'Skirts', isExpanded: false},
+]
+
+const accsCategories = [
+	{ name: 'Scarfs', isExpanded: false},
+	{ name: 'Bags', isExpanded: false},
+	{ name: 'Purses', isExpanded: false},
+	{ name: 'Jewelry', isExpanded: false}
+]
+
+const shoesCategories = [
+	{ name: 'Sneakers', isExpanded: false},
+	{ name: 'Pumps', isExpanded: false},
+	{ name: 'Stilletos', isExpanded: false},
+	{ name: 'Boots', isExpanded: false}
+]
+
+const dressRompersCategories = [ { name: 'Dresses', isExpanded: false} ]
+
+const allCategories = [
+	{group: topsCategories, name: "Tops", isExpanded: false},
+	{group: botsCategories, name: "Bottoms", isExpanded: false},
+	{group: accsCategories, name: "Accessories", isExpanded: false},
+	{group: shoesCategories, name: "Shoes", isExpanded: false},
+	{group: dressRompersCategories, name: "Dresses and Rompers", isExpanded: false}
+	]
+export default {allCategories};
