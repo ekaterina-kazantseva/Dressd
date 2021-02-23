9e9d959a8ad4ce3481bb027975bca1031d8c735c
Ekaterina Kazantseva <67483124+ezhik-v-tumane99@users.noreply.github.com>
2021-02-22 22:53:01 -0800

    Add files via upload

---
+++ src/library/lookCategories.js
@@ -0,0 +1,9 @@
+const lookCategories = [	
+	{ name: 'Casual', isExpanded: false},
+	{ name: 'Business', isExpanded: false},
+	{ name: 'Formal', isExpanded: false},
+	{ name: 'Party', isExpanded: false},
+	{ name: 'Vacation', isExpanded: false}
+]
+
+export default lookCategories;
