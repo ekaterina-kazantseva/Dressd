f39471df379f5d967609287a3b9012237e9031ec
Ekaterina Kazantseva <67483124+ekaterina-kazantseva@users.noreply.github.com>
2021-03-04 10:48:47 -0800

    Update SubCategories.js
    Added some comments and changed naming to make more readable

--- src/library/components/SubCategories.js
+++ src/library/components/SubCategories.js
@@ -3,71 +3,77 @@ import Modal from 'react-native-modal';
 import { useNavigation } from '@react-navigation/native';
 import { View, StyleSheet, TouchableOpacity, Text, FlatList, Button, TextInput } from 'react-native';
 
-export default SubCategories = ({showSubCategories, categories, onChangeCategories, index}) => {
+export default SubCategories = ({showSubCategories, subCategories, onChangeSubCategories, index}) => {
+	//State array for toggling visibility of clothing items
 	const [expandList, setExpandList] = useState([])
+	//State for toggling Add New Category modal
 	const [showModal, setShowModal] = useState(false)
+	//State for changing New Category input
 	const [inputValue, setInputValue] = useState('')
 
+	//Navigation for switching to Camera to add new item
 	const navigation = useNavigation();
+	
+	//Setting initial states for subcategories to not show their contents
 	useEffect(() => {
-		if(categories.length > 0) {
-			const initialState = categories.map(item => item.isExpanded)
+		if(subCategories.length > 0) {
+			const initialState = subCategories.map(item => item.isExpanded)
 			setExpandList(initialState)
 		}
-	}, [categories]);
+	}, [subCategories]);
 
+	//Handle press for subcategories to toggle showing their contents
 	const handlePress = (state, idx) => {
 		const newArr = [...state]
 		newArr[idx] = !newArr[idx]
 		setExpandList(newArr)
 	}
-
-	const __addCategory = () => {
-		setShowModal(true)
-	}
-
-	const submitNewCategory = () => {
-		const newCategoryName = inputValue
-		const newCategories = [...categories, { name: inputValue, isExpanded: false }]
-		onChangeCategories(newCategories, index)
+	
+	//Create new subcategory and add it to corresponding category
+	const submitNewSubCategory = () => {
+		const newSubCategoryName = inputValue
+		const newSubCategories = [...subCategories, { name: inputValue, isExpanded: false }]
+		onChangeSubCategories(newSubCategories, index)
 		setShowModal(false)
 	}
-
+	
+	//If category is set to show its subcategories
 	if (showSubCategories) {
 		return (<View>
-			<Modal isVisible={showModal} animationType="slide" transparent>
-				<View style={{height: 200, width: 300, backgroundColor: 'white', alignSelf: 'center'}}>
-					<Text>Name New Category</Text>
-					<TextInput placeholder="Name of new category" style={{ height: 40, borderColor: 'gray', borderWidth: 1}}
+				<Modal isVisible={showModal} animationType="slide" transparent>
+					<View style={style.modal}>
+						<Text>Name New Subcategory</Text>
+						<TextInput placeholder="Name of new subcategory" style={style.input}	//Modal for submitting new subcategory
 							   onChangeText={text => setInputValue(text)}
 							   value={inputValue}/>
-					<Button title="Cancel" onPress={() =>{setShowModal(false)}}/>
-					<Button title="OK" onPress={() => {submitNewCategory()}}/>
-				</View>
-			</Modal>
-			{categories.map((item, idx) => (
-				<>
-				<TouchableOpacity onPress={() => handlePress(expandList, idx)}>
-				<Text style={style.subMenuText}>{item.name}</Text>
-				</TouchableOpacity>
-
-				{expandList[idx] && <FlatList showsHorizontalScrollIndicator={false} horizontal
-				data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'e' }, { key: 'f' }]}
-											  renderItem={({item, index}) => (<View style={style.renderItem}>
-												  <TouchableOpacity onPress={() => navigation.navigate('Camera')}>
-													  {index == 0 && <Text style={style.subMenuText}>+Add New Item</Text>}
-												  </TouchableOpacity>
-											  </View>)}/> }
-				</>
+						<Button title="Cancel" onPress={() =>{setShowModal(false)}}/>
+						<Button title="OK" onPress={() => {submitNewSubCategory()}}/>
+					</View>
+				</Modal>
+				{subCategories.map((item, idx) => (					
+					<>	//renders all subcategories as buttons
+						<TouchableOpacity onPress={() => handlePress(expandList, idx)}>
+							<Text style={style.subMenuText}>{item.name}</Text>
+						</TouchableOpacity>
+						//if subcategory is clicked, shows its contents
+						{expandList[idx] && 
+						 <FlatList showsHorizontalScrollIndicator={false} horizontal
+							   data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }, { key: 'e' }, { key: 'f' }]}
+							   renderItem={({item, index}) => (
+								<View style={style.renderItem}>
+									<TouchableOpacity onPress={() => navigation.navigate('Camera')}>
+									{index == 0 && <Text style={style.subMenuText}>+Add New Item</Text>}
+									</TouchableOpacity>
+								</View>
+						)}/>}
+					</>
 				))}
-
-			<TouchableOpacity onPress={() => __addCategory()}>
-				<Text style={style.subMenuText}>+Add New Category</Text>
-			</TouchableOpacity>
-			</View>)
+				<TouchableOpacity onPress={() => setShowModal(true)}>
+					<Text style={style.subMenuText}>+Add New Subcategory</Text>
+				</TouchableOpacity>
+		</View>)
 	}
-
-	return null
+	return null //else show nothing
 }
 
 const style = StyleSheet.create({
@@ -76,7 +82,7 @@ const style = StyleSheet.create({
 		fontSize: 20,
 		color: 'rgba(0,0,0,0.5)',
 		paddingLeft: 20,
-		paddingTop: 5
+		paddingTop: 5,
 	},
 	renderItem: {
 		borderBottomWidth: 1,
@@ -87,5 +93,16 @@ const style = StyleSheet.create({
 		height: 100,
 		maxHeight:100,
 		backgroundColor: '#fff',
-	}
+	},
+	modal: {
+		height: 200, 
+		width: 300, 
+		backgroundColor: 'white', 
+		alignSelf: 'center',
+	},
+	input: {
+		height: 40, 
+		borderColor: 'gray', 
+		borderWidth: 1,
+	},
 });
